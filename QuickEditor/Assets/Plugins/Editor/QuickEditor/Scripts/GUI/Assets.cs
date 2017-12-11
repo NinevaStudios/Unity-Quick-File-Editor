1269f48ce6b1ae5c287d82fb508ee952e9b270f3
Taras Leskiv <leskiv.taras@gmail.com>
2017-12-11 01:01:01 +0100

    cleanup

--- QuickEditor/Assets/Plugins/Editor/QuickEditor/Scripts/GUI/Assets.cs
+++ QuickEditor/Assets/Plugins/Editor/QuickEditor/Scripts/GUI/Assets.cs
@@ -60,14 +60,6 @@ namespace DeadMosquito.QuickEditor
 			public static readonly Texture2D SaveTexture;
 			public static readonly Texture2D MoreOptionsTexture;
 
-			public static readonly Texture2D LemonNoteTexture;
-			public static readonly Texture2D GrassNoteTexture;
-			public static readonly Texture2D SkyBlueNoteTexture;
-			public static readonly Texture2D AmethystNoteTexture;
-			public static readonly Texture2D RoseNoteTexture;
-			public static readonly Texture2D CleanNoteTexture;
-
-			public static readonly Texture HasText;
 
 			static Dictionary<NoteColor, Texture2D> _notes;
 
@@ -77,36 +69,6 @@ namespace DeadMosquito.QuickEditor
 				CloseTexture = GetTexture("ic_close");
 				SaveTexture = GetTexture("ic_save");
 				MoreOptionsTexture = GetTexture("ic_color_picker");
-
-				LemonNoteTexture = GetTexture("1x/lemon");
-				GrassNoteTexture = GetTexture("1x/grass");
-				SkyBlueNoteTexture = GetTexture("1x/sky_blue");
-				AmethystNoteTexture = GetTexture("1x/amethyst");
-				RoseNoteTexture = GetTexture("1x/rose");
-				CleanNoteTexture = GetTexture("1x/clean");
-
-				HasText = GetTexture("has_text");
-
-				InitNotesDic();
-			}
-
-
-			static void InitNotesDic()
-			{
-				_notes = new Dictionary<NoteColor, Texture2D>
-				{
-					{NoteColor.Lemon, LemonNoteTexture},
-					{NoteColor.Grass, GrassNoteTexture},
-					{NoteColor.SkyBlue, SkyBlueNoteTexture},
-					{NoteColor.Amethyst, AmethystNoteTexture},
-					{NoteColor.Rose, RoseNoteTexture},
-					{NoteColor.Clean, CleanNoteTexture}
-				};
-			}
-
-			public static Texture2D NoteByColor(NoteColor color)
-			{
-				return !_notes.ContainsKey(color) ? LemonNoteTexture : _notes[color];
 			}
 
 			static Texture2D GetTexture(string name)
